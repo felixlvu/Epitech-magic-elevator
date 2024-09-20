85b1a0f7c1bb376c62696999bb72c342d1fc21a1
Felix schrynemaeker <felix.schrynemaekers@epitech.eu>
2024-09-20 09:41:12 +0200

    ADD : Base docs for api

---
+++ api/docs/swaggerConfig.js
@@ -0,0 +1,27 @@
+import swaggerJSDoc from "swagger-jsdoc";
+const port = process.env.PORT || 3000;
+
+const swaggerDefinition = {
+  openapi: "3.1.0",
+  info: {
+    title: "Express API",
+    version: "1.0.0",
+    description: "Utilisation de l'API",
+  },
+  servers: [
+    {
+      url: `http://localhost:${port}`,
+      description: "Serveur de développement",
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Chemins vers les fichiers contenant des annotations Swagger
+  apis: ["./docs/routes/**/*.yaml"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
